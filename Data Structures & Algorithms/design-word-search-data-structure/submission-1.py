b35028310250e45c3d3d425d240a81823bf2fa40
Hardik Lad <62031121+HardikLad10@users.noreply.github.com>
2026-05-28 20:55:42 -0500

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,42 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {} # eg a: TrieNode
+        self.word = False # to check if endOfWord
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root # initially curr var is root val(placeholder)
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode() # in the dict -> c : TrieNode()
+            # shift ptr
+            cur = cur.children[c]
+        cur.word = True # this indicated endOfWord
+
+    def search(self, word: str) -> bool:
+        
+        def dfs(j, root):
+            # init cur as root placeholder
+            cur = root 
+
+            for i in range(j, len(word)): 
+                c = word[i]
+                if c == '.':
+                    # check for all children of cur c, ie. all vals in dict
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.word
+        
+        return dfs(0, self.root)
